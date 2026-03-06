d67f200542558157adce46d1cfe3816094c289ff
Roberto <r.antonio1827@gmail.com>
2026-03-06 19:40:55 +0100

    fix: prevenir libros duplicados por ISBN 1. Añadir validacion en addBook() 2. Crear metodo findBookByISBN() 3. Añadir tests unitarios 3. Close 1

---
+++ E2-library-buggy/LibraryTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibraryTest {
+
+    @Test
+    public void testAddDuplicateBook() {
+        Library lib = new Library();
+
+        Book b1 = new Book("Titulo", "Autor", "ISBN-123");
+        Book b2 = new Book("Titulo", "Autor", "ISBN-123");
+
+        lib.addBook(b1);
+        lib.addBook(b2);
+
+        // Aquí comprobamos el tamaño
+        assertEquals(1, lib.getBooks().size());
+    }
+}
