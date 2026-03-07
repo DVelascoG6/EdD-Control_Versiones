c7208329ba4f751a778cb9f53c5b3600109ce8ee
BertaP-H <berta.pena.hernandez@gmail.com>
2026-03-07 16:16:11 +0100

    refactor: refactorizar NotificationManager

--- P4.1-notifications-base/NotificationManager.java
+++ P4.1-notifications-base/NotificationManager.java
@@ -1,26 +1,28 @@
-// NotificationManager.java - Código a refactorizar
 public class NotificationManager {
-    // TODO: Separar en clases diferentes: EmailService, SMSService, PushService
-    // TODO: Aplicar patrón Strategy para los tipos de notificación
-    // TODO: Añadir sistema de logs
-    
-    public void send(String type, String message, String recipient) {
-        if (type.equals("email")) {
-            // Código para enviar email
-            System.out.println("Enviando email a " + recipient + ": " + message);
-            // Lógica compleja de email aquí...
-        } else if (type.equals("sms")) {
-            // Código para enviar SMS
-            System.out.println("Enviando SMS a " + recipient + ": " + message);
-            // Lógica compleja de SMS aquí...
-        } else if (type.equals("push")) {
-            // Código para notificación push
-            System.out.println("Enviando push a " + recipient + ": " + message);
-            // Lógica compleja de push aquí...
+
+    private NotificationService emailService;
+    private NotificationService smsService;
+    private NotificationService pushService;
+
+    public NotificationManager() {
+        this.emailService = new EmailService();
+        this.smsService = new SMSService();
+        this.pushService = new PushService();
+    }
+
+    public void sendNotification(String type, String recipient, String message) {
+        switch (type.toLowerCase()) {
+            case "email":
+                emailService.send(recipient, message);
+                break;
+            case "sms":
+                smsService.send(recipient, message);
+                break;
+            case "push":
+                pushService.send(recipient, message);
+                break;
+            default:
+                System.out.println("Tipo de notificación no reconocido: " + type);
         }
     }
-    
-    // TODO: Añadir método para enviar a múltiples destinatarios
-    // TODO: Añadir sistema de reintentos
-    // TODO: Añadir validación de parámetros
 }
