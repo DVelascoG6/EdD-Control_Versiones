7c266f37c2d74bea051b9c7a7583f2cce1238d4b
CachopoDabuela <233050081+CachopoDabuela@users.noreply.github.com>
2026-03-07 20:10:22 +0100

    crear SMSService y extraer lógica de sms a clase SMSService

---
+++ P4.1-notifications-base/SMSService.java
@@ -0,0 +1,6 @@
+public class SMSService implements NotificationService {
+    @Override
+    public void send(String message, String recipient) {
+        System.out.println("Enviando SMS a " + recipient + ": " + message);
+    }
+}
