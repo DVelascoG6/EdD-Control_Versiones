8588768517caa4d1148cb7ab8c7fc8aa141e2f28
jorge <shalome@bendiciones.com>
2026-03-08 21:34:39 +0000

    feat: Sistema de validacion de mensaje y destinatario añadido

--- P4.1-notifications-base/NotificationManager.java
+++ P4.1-notifications-base/NotificationManager.java
@@ -13,6 +13,7 @@ public class NotificationManager {
             throw new IllegalStateException("\n[LOG] Estrategia no definida!.");
 
         int attempts = 0;
+        validate(message, recipient);
 
         while (attempts < MAX_RETRIES)
         {
@@ -23,16 +24,22 @@ public class NotificationManager {
                 return;
             } catch (Exception e) 
             {
-                System.out.println("\n[LOG] Fallo al enviar la notificación. Intento: " + attempts + ". Error: + " + e.getMessage());  
+                System.out.println("\n[LOG] Fallo al enviar la notificación. Intento: " + attempts + ". Error: " + e.getMessage());  
                 
                 // Practica básica, no implemento delay para rate-limit
-
                 // Si ya agotamos los 3 intentos
                 if (attempts == MAX_RETRIES)
                     throw new RuntimeException("\n[LOG] No se pudo enviar la notificación después de " + MAX_RETRIES + " intentos");
             }
         }
     }
+
+    private void validate(String message, String recipient) {
+    if (message == null) throw new IllegalArgumentException("\n[!] Mensaje null!!.");
+    if (recipient == null) throw new IllegalArgumentException("\n[!] Destinatario null!!.");
+    if (message.trim().isEmpty()) throw new IllegalArgumentException("\n[!] Mensaje vacío!!.");
+    if (recipient.trim().isEmpty()) throw new IllegalArgumentException("\n[!] Destinatario vacío!!.");
+}
 
     // NotificationManager.java - Código a refactorizar
     // TODO: Separar en clases diferentes: EmailService, SMSService, PushService
