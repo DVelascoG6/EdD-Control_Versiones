0de102841bf4624ff70b642a36bb100bef0a784b
BertaP-H <berta.pena.hernandez@gmail.com>
2026-03-14 19:30:14 +0100

    fix: corregir bugs en Book - getters y validación de préstamo
    Closes #9
    Closes #10
    Closes #11

--- E2-library-buggy/Book.java
+++ E2-library-buggy/Book.java
@@ -4,26 +4,41 @@ public class Book {
     private String author;
     private String isbn;
     private boolean available;
-    
+
     public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.available = true;
     }
-    
-    // BUG 1: No hay getters/setters para todos los campos
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    // Faltan getIsbn() y isAvailable()
-    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
     public void borrow() {
-        // BUG 2: No valida si ya está prestado
+        if (!available) {
+            throw new IllegalStateException("El libro ya está prestado");
+        }
         available = false;
     }
-    
+
     public void returnBook() {
-        // BUG 3: No valida si ya estaba disponible
+        if (available) {
+            throw new IllegalStateException("El libro ya estaba disponible");
+        }
         available = true;
     }
 }
