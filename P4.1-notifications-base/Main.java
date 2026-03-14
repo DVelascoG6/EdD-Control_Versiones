a7690dbce9e8271a64bf488e545171e6e0cd4205
jackie102 <124812840+jackie102@users.noreply.github.com>
2026-03-14 13:20:18 +0100

    Update email notification message

--- P4.1-notifications-base/Main.java
+++ P4.1-notifications-base/Main.java
@@ -4,7 +4,7 @@ public class Main {
         NotificationManager manager = new NotificationManager();
         
         // Ejemplos de uso
-        manager.send("email", "Bienvenido al sistema", "usuario@email.com");
+        manager.send("email", "miau al sistema", "usuario@email.com");
         manager.send("sms", "Tu código es 3321", "+34123456789");
         manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
     }
