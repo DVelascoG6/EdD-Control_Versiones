b2327cfe6c4bc95a41024b8dd3d3bad1a41e97b7
BertaP-H <berta.pena.hernandez@gmail.com>
2026-03-14 19:57:43 +0100

    fix: actualizar LibraryApp con los bugs corregidos

--- E2-library-buggy/LibraryApp.java
+++ E2-library-buggy/LibraryApp.java
@@ -3,16 +3,16 @@ public class LibraryApp {
     public static void main(String[] args) {
         // Creamos una nueva librería
         Library library = new Library();
-        
+
         // Agregar nuestros libros favoritos
         library.addBook(new Book("Clean Code", "Robert Martin", "978-0132350884"));
         library.addBook(new Book("Clean Code", "Robert Martin", "978-0132350884")); // Duplicado
         library.addBook(new Book("Design Patterns", "Gamma et al.", "978-0201633610"));
-        
+
         // BUG: Intentar buscar con diferente capitalización
         Book found = library.findBookByTitle("clean code");
         System.out.println("Encontrado: " + (found != null ? found.getTitle() : "null"));
-        
+
         // Comprobamos el préstamo
         Book book = library.findBookByTitle("Clean Code");
         if (book != null) {
@@ -21,7 +21,7 @@ public class LibraryApp {
             book.returnBook();
             book.returnBook(); // BUG: Devolver dos veces
         }
-        
+
         // Listar disponibles
         System.out.println("Libros disponibles: " + library.findAvailableBooks().size());
     }
